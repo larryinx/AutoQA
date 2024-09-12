04780d0e769f40be5764c7eff7677a75e90703cf
Larryinx <1155160255@link.cuhk.edu.hK>
2024-09-12 18:43:12 -0400

    fixed download

--- pre_train_model/download.py
+++ pre_train_model/download.py
@@ -2,5 +2,5 @@
 from modelscope import snapshot_download
 
 qwen = snapshot_download('qwen/Qwen-7B-Chat', local_dir='./pre_train_model/Qwen-7B-Chat')
-m3e = snapshot_download('m3e-large', local_dir='./pre_train_model/m3e-large')
-bge = snapshot_download('bge-reranker-large', local_dir='./pre_train_model/bge-reranker-large')
+m3e = snapshot_download('AI-ModelScope/m3e-large', revision='master', local_dir='./pre_train_model/m3e-large')
+bge = snapshot_download('AI-ModelScope/bge-large-zh-v1.5', local_dir='./pre_train_model/bge-reranker-large')
